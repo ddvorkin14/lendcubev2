4e0d7623045bb06b21799723785d2112bb91f4d7
Daniel Dvorkin <ddvorkin@joinhandshake.com>
2022-04-26 23:09:11 -0400

    added datatables

--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card, Container, Table, Button } from "react-bootstrap";
+import { Card, Container, Table, Button, Tabs, Tab } from "react-bootstrap";
+import DataTable from 'react-data-table-component';
+import Moment from 'moment';
 
 const Loans = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [pagination, setPagination] = useState({});
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     var config = {
@@ -17,13 +20,13 @@ const Loans = () => {
         password: 'passwordA1'
       }
     }
-    axios.get('https://app.lendcube.ca/api/v1/loans?page=' + page, config).then((resp) => {
+    axios.get('http://localhost:5001/api/v1/loans?page=' + page + "&search=" + query, config).then((resp) => {
       setData(resp.data.loans);
       setPagination(resp.data.pagination);
       setLoading(false);
     });
     
-  }, [page]);
+  }, [page, query]);
 
   const nextPage = () => {
     setPage(page + 1);
@@ -32,6 +35,42 @@ const Loans = () => {
   const prevPage = () => {
     setPage(page - 1);
   }
+
+  const ExpandedComponent = ({ data }) => {
+    return(
+      <div className="pt-2" style={{float:'left'}}>
+        <Tabs
+          defaultActiveKey="address"
+          transition={false}
+          id="noanim-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="address" title="Address Details">
+            <div style={{textAlign: 'left', marginLeft: '20px' }}>
+              <h3>Address:</h3>
+              <p>{JSON.stringify(data)}</p>    
+            </div>
+          </Tab>
+          <Tab eventKey="settings" title="Settings">
+            <div style={{textAlign: 'left', marginLeft: '20px' }}>
+              <h3>Configuration: </h3>
+              <p>{JSON.stringify(data)}</p>    
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+      
+    )
+  }
+
+  const columns = [
+    { name: 'First Name', selector: row => row.first_name, sortable: true },
+    { name: 'Last Name', selector: row => row.last_name, sortable: true },
+    { name: 'Email', selector: row => row.customer_email, sortable: true },
+    { name: 'Country', selector: row => row.country, sortable: true },
+    { name: 'Amount', selector: row => `$${row.amount}`, sortable: true },
+    { name: 'Created', selector: row => `${Moment(row.created_at).format("L LT")}`, sortable: true }
+  ];
 
   return(
     <Container className="pt-4 pb-4">
@@ -58,7 +97,11 @@ const Loans = () => {
           </div>
         </Card.Body>
         <Card.Body>
-          <Table striped bordered hover>
+
+          <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        </Card.Body>
+        <Card.Body>
+          {/* <Table striped bordered hover>
             <thead>
               <tr>
                 <th>ID</th>
@@ -81,7 +124,15 @@ const Loans = () => {
                 )
               })}
             </tbody>
-          </Table>
+          </Table> */}
+          <DataTable
+            columns={columns}
+            data={data}
+            selectableRows
+            dense
+            expandableRows
+            expandableRowsComponent={ExpandedComponent}
+        />
         </Card.Body>
       </Card>
     </Container>
