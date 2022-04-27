7e12262a020b9ac59b3e5af901772670a29532b1
Daniel Dvorkin <ddvorkin@joinhandshake.com>
2022-04-26 22:09:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Loans from "./pages/Loans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Lendcube Admin</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/loans">Loans</Nav.Link>
+                
+                <NavDropdown title="Settings" id="basic-nav-dropdown" disabled>
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
+        <Routes>
+          <Route path="/loans" element={<Loans/>} />
+          <Route path="/" element={<Loans />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Loans.jsx
@@ -0,0 +1,91 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, Container, Table, Button } from "react-bootstrap";
+
+const Loans = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pagination, setPagination] = useState({});
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    var config = {
+      responseType: 'json',
+      format: 'json',
+      auth: {
+        username: 'dvorkin212@gmail.com',
+        password: 'passwordA1'
+      }
+    }
+    axios.get('https:/app.lendcube.ca/api/v1/loans?page=' + page, config).then((resp) => {
+      setData(resp.data.loans);
+      setPagination(resp.data.pagination);
+      setLoading(false);
+    });
+    
+  }, [page]);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  }
+
+  const prevPage = () => {
+    setPage(page - 1);
+  }
+
+  return(
+    <Container className="pt-4 pb-4">
+      <Card>
+        {loading && (
+          <p>Loading Data</p>
+        )}
+        <Card.Header align="start">Loans</Card.Header>
+        <Card.Body>
+          <div style={{display: 'flex', float: 'left'}}>
+            <p className="pl-2">Total Records: {pagination?.total_entries}</p>
+          </div>
+          <div style={{display: 'flex', float: 'right'}}>
+            <p className="pr-2">Current Page: {pagination?.current_page}</p>
+          </div>
+        </Card.Body>
+        <Card.Body>
+          <div style={{display: 'flex', float: 'left'}}>
+            <Button disabled={page === 1} onClick={() => prevPage()}>Previous</Button>
+          </div>
+          
+          <div style={{display: 'flex', float: 'right'}}>
+            <Button disabled={data.length < pagination.per_page} onClick={() => nextPage()}>Next</Button>
+          </div>
+        </Card.Body>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Created By</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 && data.map((d) => {
+                return(        
+                  <tr>
+                    <td>{d.id}</td>
+                    <td>{d.first_name}</td>
+                    <td>{d.last_name}</td>
+                    <td>{d.customer_email}</td>
+                    <td>{d.created_by.email}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
+};
+
+export default Loans;
