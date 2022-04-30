4ab4cfe24067648c74ef4f2e8ee96167b5e67ccc
Daniel Dvorkin <ddvorkin@joinhandshake.com>
2022-04-30 03:34:15 -0400

    new loan stuff

--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -16,8 +16,6 @@ const Loans = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [dataLoading, setDataLoading] = useState(true);
-  const [pagination, setPagination] = useState({});
-  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
@@ -29,9 +27,8 @@ const Loans = () => {
 
   useEffect(() => {
     if(localStorage?.token?.length > 10){
-      axios.get('https://app.lendcube.ca/api/v1/loans?page=' + page + "&search=" + query, authHeader).then((resp) => {
+      axios.get('https://app.lendcube.ca/api/v1/loans?search=' + query, authHeader).then((resp) => {
         setData(resp.data.loans);
-        setPagination(resp.data.pagination);
         setLoading(false);
         setDataLoading(false);
       }).catch(function (error) {
@@ -43,28 +40,18 @@ const Loans = () => {
       navigate("/login");
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, query]);
-
-  const nextPage = () => {
-    setDataLoading(true);
-    setPage(page + 1);
-  }
-
-  const prevPage = () => {
-    setDataLoading(true);
-    setPage(page - 1);
-  }
+  }, [query]);
 
   const columns = [
     { name: '', width: '60px', selector: row => <Button minimal={true} icon="eye-open" onClick={() => navigate("/loans/" + row.id) } />, sortable: false },
-    { name: 'ID', width: '100px', selector: row => `0000${row.id}`, sortable: false },
+    { name: 'ID', width: '100px', selector: row => `${row.id}`, sortable: false },
     { name: 'First Name', selector: row => row.first_name, sortable: true },
     { name: 'Last Name', selector: row => row.last_name, sortable: true },
     { name: 'Email', width: '250px', selector: row => row.customer_email, sortable: true },
     { name: 'Country', width: '100px', selector: row => row.country, sortable: true },
     { name: 'Amount', width: '120px', selector: row => <CurrencyFormat value={row.amount} displayType={'text'} decimalScale="2" fixedDecimalScale={true} thousandSeparator={true} prefix={'$'} />, sortable: false },
     { name: 'Frequency', width: '110px', selector: row => row.frequency, sortable: true },
-    { name: 'Created', selector: row => `${Moment(row.created_at).format("L LT")}`, sortable: true }
+    { name: 'Created', width: '170px', selector: row => `${Moment(row.created_at).format("L LT")}`, sortable: true }
   ];
 
 
@@ -82,34 +69,13 @@ const Loans = () => {
         
         <Card.Body>
           <Row>
-            <Col>
-              <div style={{float: 'left', textAlign: 'left'}}>
-                <p className={`${dataLoading ? Classes.SKELETON : ''}`}>
-                  Total Records: {pagination?.total_entries}
-                  <br/>
-                  Current Page: {pagination?.current_page}
-                </p>
-              </div>
-            </Col>
-
-            <Col>
-              <input className={loading ? Classes.SKELETON : ''} type="text" value={query} placeholder="Search...." onChange={(e) => {
+            <Col lg={4}>
+              <input style={{width: '100%'}} type="search" className={loading ? Classes.SKELETON : ''} value={query} placeholder="Search for a Loan...." onChange={(e) => {
                 setQuery(e.target.value);
-                setPage(1);
                 setDataLoading(true);
               }} />
             </Col>
-
-            <Col>
-              <div style={{float: 'right'}}>
-                <ButtonGroup minimal={false}>
-                  <Button className={`${loading ? Classes.SKELETON : ''} mr-2`} disabled={page === 1} onClick={() => prevPage()}>Previous</Button>
-                  <Button className={loading ? Classes.SKELETON : ''} disabled={data.length < pagination.per_page} onClick={() => nextPage()}>Next</Button>
-                </ButtonGroup>
-              </div>
-            </Col>
           </Row>
-          
         </Card.Body>
 
         <Card.Body>
@@ -118,6 +84,7 @@ const Loans = () => {
               columns={columns}
               data={data}
               selectableRows
+              pagination
               // expandableRows
               expandableRowsComponent={ExpandedComponent}
           />
--- src/pages/NewLoan.jsx
+++ src/pages/NewLoan.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Form, Row } from "react-bootstrap";
-import { Button, Classes, Divider, FormGroup, InputGroup, Position, Toaster } from "@blueprintjs/core";
+import { Button, Classes, Divider, FormGroup, InputGroup, Position, Toast, Toaster } from "@blueprintjs/core";
 import { DateInput } from "@blueprintjs/datetime";
 import Checklist from "../components/Checklist";
 import moment from "moment";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AppToaster = Toaster.create({
   className: "recipe-toaster",
@@ -12,22 +13,33 @@ const AppToaster = Toaster.create({
   maxToasts: 2
 });
 
+const authHeader = {
+  headers: {
+    'Authorization': `Bearer ${localStorage.token}`
+  }
+}
+
 const NewLoan = () => {
   const [loading, setLoading] = useState(true);
   const [allPossibleUsers, setAllPossibleUsers] = useState([]);
   const navigate = useNavigate();
+  const [newLoanErrors, setNewLoanErrors] = useState([]);
   const [newLoan, setNewLoan] = useState({
-    frequency: 'Monthly', 
-    serviceUse: 'Personal',
-    country: 'Canada'
+    frequency: 'Monthly', service_use: 'Personal',
+    country: 'Canada', created_by_id: allPossibleUsers.length > 0 ? allPossibleUsers[0] : ''
   });
 
   useEffect(() => {
-    
-
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+    if(localStorage?.token?.length > 10){
+      axios.get('https://app.lendcube.ca/api/v1/users', authHeader).then((resp) => {
+        setAllPossibleUsers(resp.data.users);
+        setLoading(false)
+      }).catch((e) => {
+        navigate("/login");
+      });
+    } else {
+      navigate("/login");
+    }
   }, []);
 
   const onChange = (e) => {
@@ -36,16 +48,16 @@ const NewLoan = () => {
   }
 
   const verifyCustomerDetails = () => {
-    return !!(newLoan?.firstName && 
-      newLoan?.lastName && 
-      newLoan?.customerEmail && 
-      newLoan?.phoneNumber)
+    return !!(newLoan?.first_name && 
+      newLoan?.last_name && 
+      newLoan?.customer_email && 
+      newLoan?.customer_phone)
   }
 
   const verifyCustomerAddress = () => {
     return !!(newLoan?.address1 && 
       newLoan?.city && 
-      newLoan?.postalCode && 
+      newLoan?.postalcode && 
       newLoan?.province && 
       newLoan?.country)
   }
@@ -53,63 +65,80 @@ const NewLoan = () => {
   const verifyLoanDetails = () => {
     return !!(
       newLoan?.frequency &&
-      newLoan?.serviceUse &&
+      newLoan?.service_use &&
       newLoan?.amount &&
-      newLoan?.startDate
+      newLoan?.start_date
     )
   }
 
   const allFieldsValid = () => {
-    return verifyCustomerDetails && verifyCustomerAddress && verifyLoanDetails
+    return verifyCustomerDetails() && verifyCustomerAddress() && verifyLoanDetails()
   }
 
   const resetLoan = () => {
     setNewLoan({
-      firstName: '', lastName: '', customerEmail: '', phoneNumber: '',
-      address1: '', address2: '', city: '', postalCode: '',
-      frequency: 'Monthly', serviceUse: 'Personal', amount: '', startDate: null
+      first_name: '', last_name: '', customer_email: '', customer_phone: '',
+      address1: '', address2: '', city: '', postalcode: '',
+      frequency: 'Monthly', service_use: 'Personal', amount: '', start_date: null
     });
     AppToaster.show({ message: 'Loan has been successfully reset back to default', intent: 'success' });
   }
 
+  const saveLoan = () => {
+    if(allFieldsValid()){
+      axios.post("https://app.lendcube.ca/api/v1/loans", { new_loan: newLoan }).then((resp) => {
+        if(!!resp.data.errors){
+          Object.keys(resp.data.errors).map((key) => {
+            return AppToaster.show({ message: `Loan did not save due to: ${key.replace("_", " ") + ": " + resp.data.errors[key] }`, intent: 'danger' });
+          })
+        } else {
+          AppToaster.show({ message: `Loan has been successfully created.`, intent: 'success' });
+          navigate("/loans/" + resp.data.loan.id);
+        }
+      })
+    } else {
+      AppToaster.show({ message: 'Loan cannot be saved due to invalid data. Please refer to the checklist and try again.', intent: 'danger' });
+    }
+  }
+
   const fields = [
-    { id: 1, label: 'First Name', field: 'firstName', tabIndex: 1, required: true, type: 'text' },
-    { id: 2, label: 'Last Name', field: 'lastName', tabIndex: 2, required: true, type: 'text' },
-    { id: 3, label: 'Customer Email', field: 'customerEmail', tabIndex: 3, required: true, type: 'text' },
-    { id: 4, label: 'Phone #', field: 'phoneNumber', tabIndex: 4, required: true, type: 'text' },
+    { id: 1, label: 'First Name', field: 'first_name', tabIndex: 1, required: true, type: 'text' },
+    { id: 2, label: 'Last Name', field: 'last_name', tabIndex: 2, required: true, type: 'text' },
+    { id: 3, label: 'Customer Email', field: 'customer_email', tabIndex: 3, required: true, type: 'text' },
+    { id: 4, label: 'Phone #', field: 'customer_phone', tabIndex: 4, required: true, type: 'text' },
     { id: 5, label: 'divider' },
     { id: 6, label: 'Address 1', field: 'address1', tabIndex: 5, required: true, type: 'text' },
     { id: 7, label: 'Address 2', field: 'address2', tabIndex: 6, required: false, type: 'text' },
     { id: 8, label: 'City', field: 'city', tabIndex: 7, required: true, type: 'text' },
-    { id: 9, label: 'Postal Code', field: 'postalCode', tabIndex: 8, required: true, type: 'text' },
+    { id: 9, label: 'Postal Code', field: 'postalcode', tabIndex: 8, required: true, type: 'text' },
     { id: 10, label: 'Province', field: 'province', tabIndex: 9, required: true, type: 'text' },
     { id: 11, label: 'Country', field: 'country', tabIndex: 10, required: true, type: 'text' },
     { id: 12, label: 'divider' },
     { id: 13, label: 'Frequency', field: 'frequency', tabIndex: 11, required: true, type: 'select', options: ["Monthly", "Bi-Weekly"] },
-    { id: 14, label: 'Account Type', field: 'serviceUse', tabIndex: 12, required: true, type: 'select', options: ["Personal", "Business"] },
+    { id: 14, label: 'Account Type', field: 'service_use', tabIndex: 12, required: true, type: 'select', options: ["Personal", "Business"] },
     { id: 15, label: 'Amount ($)', field: 'amount', tabIndex: 13, required: true, type: 'text' },
-    { id: 16, label: 'Start Date', field: 'startDate', tabIndex: 14, required: true, type: 'date' },
+    { id: 16, label: 'Start Date', field: 'start_date', tabIndex: 14, required: true, type: 'date' },
   ]
 
   const checklist = [
     { id: 1, label: 'Name, Email and Phone #: Verified', func: verifyCustomerDetails, list: [
-      { id: 1, label: 'first name', func: newLoan?.firstName },
-      { id: 2, label: 'last name', func: newLoan?.lastName },
-      { id: 3, label: 'customer email', func: newLoan?.customerEmail },
-      { id: 4, label: 'phone number', func: newLoan?.phoneNumber },
+      { id: 1, label: 'first name', func: newLoan?.first_name },
+      { id: 2, label: 'last name', func: newLoan?.last_name },
+      { id: 3, label: 'customer email', func: newLoan?.customer_email },
+      { id: 4, label: 'phone number', func: newLoan?.customer_phone },
     ] },
     { id: 2, label: 'Customer Address: Verified', func: verifyCustomerAddress, list: [
       { id: 1, label: 'address 1', func: newLoan?.address1 },
       { id: 2, label: 'city', func: newLoan?.city },
-      { id: 3, label: 'postal code', func: newLoan?.postalCode },
+      { id: 3, label: 'postal code', func: newLoan?.postalcode },
       { id: 4, label: 'province', func: newLoan?.province },
       { id: 5, label: 'country', func: newLoan?.country },
     ]},
     { id: 3, label: 'Loan Details: Verified', func: verifyLoanDetails, list: [
       { id: 1, label: 'frequency', func: newLoan?.frequency },
-      { id: 2, label: 'account type', func: newLoan?.serviceUse },
+      { id: 2, label: 'account type', func: newLoan?.service_use },
       { id: 3, label: 'loan amount', func: newLoan?.amount },
-      { id: 4, label: 'start date', func: newLoan?.startDate },
+      { id: 4, label: 'start date', func: newLoan?.start_date },
     ] }
   ]
 
@@ -123,6 +152,11 @@ const NewLoan = () => {
 
   return (
     <Container className="pt-4 pb-4 loans-container">
+      {newLoanErrors.length > 0 && (
+        newLoanErrors?.map((error) => {
+          return <Toast intent='danger' message={error} timeout={15000} position={Position.TOP}/>
+        })
+      )}
       <Card>
         <Card.Header align="start" className={loading ? Classes.SKELETON : ''}>
           New Loan
@@ -159,7 +193,7 @@ const NewLoan = () => {
                               label={input.label}
                               labelFor="text-input"
                               labelInfo={input.required ? <span style={{color: 'red'}}>*</span> : ''}>
-                                <Form.Select size="sm" tabIndex={input.tabIndex} onChange={(e) => onChange(e)} value={newLoan[input.field]}>
+                                <Form.Select size="sm" tabIndex={input.tabIndex} name={input.field} onChange={(e) => onChange(e)} value={newLoan[input.field]}>
                                   {input.options.map((option) => {
                                     return <option key={option}>{option}</option>
                                   })}
@@ -199,9 +233,10 @@ const NewLoan = () => {
                 label="Processed By"
                 labelFor="text-input"
                 labelInfo={<span style={{color: 'red'}}>*</span>}>
-                  <Form.Select size="sm" tabIndex={15} onChange={(e) => onChange(e)} value={newLoan?.createdBy}>
-                    {allPossibleUsers.map((option) => {
-                      return <option key={option}>{option}</option>
+                  <Form.Select size="sm" tabIndex={15} name="created_by_id" onChange={(e) => onChange(e)} value={newLoan?.created_by_id}>
+                    <option>-- Select User --</option>
+                    {allPossibleUsers?.map((option) => {
+                      return <option key={option.id} value={option.id}>{option.email}</option>
                     })}
                   </Form.Select>
               </FormGroup>
@@ -212,7 +247,7 @@ const NewLoan = () => {
           <div style={{float: 'right'}}>
             <Button icon={"undo"} intent="default" style={{marginRight: 5}} onClick={() => navigate("/loans") }>Cancel</Button>
             <Button icon={"reset"} intent="warning" style={{marginRight: 5}} onClick={() => resetLoan() }>Reset Loan</Button>
-            <Button icon={"saved"} intent="success" disabled={!allFieldsValid}>Create Loan</Button>
+            <Button icon={"saved"} intent="success" onClick={() => saveLoan()}>Create Loan</Button>
           </div>
         </Card.Footer>
       </Card>
