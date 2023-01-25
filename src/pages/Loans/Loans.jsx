a50ca9c94f7be3cc8a1d55cf59051a698ee18ed4
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2023-01-25 00:52:48 -0500

    made some loans page updates:

--- src/pages/Loans/Loans.jsx
+++ src/pages/Loans/Loans.jsx
@@ -3,14 +3,16 @@ import React, { useEffect, useState } from "react";
 import { Card, Row, Col } from "react-bootstrap";
 import DataTable from 'react-data-table-component';
 import Moment from 'moment';
-import { Classes, Button } from "@blueprintjs/core";
+import { Classes, Button, MenuItem } from "@blueprintjs/core";
 import { useNavigate } from "react-router-dom";
 import CurrencyFormat from "react-currency-format";
 import Layout from "../../components/Layout";
+import { Select2 } from "@blueprintjs/select";
 
 const BREADCRUMBS = [
   { href: "/loans", icon: "folder-close", text: "Loans" }
 ];
+
 
 const Loans = () => {
   const [data, setData] = useState([]);
@@ -18,13 +20,26 @@ const Loans = () => {
   const [dataLoading, setDataLoading] = useState(true);
   const [query, setQuery] = useState("");
   const [selectedFilter, setSelectedFilter] = useState("all")
+  const [stores, setStores] = useState([]);
   const navigate = useNavigate();
+  const [selectedStore, setSelectedStore] = useState({});
 
   const authHeader = {
     headers: {
       'Authorization': `Bearer ${localStorage.token}`
     }
   }
+
+  const filterStores = (query, store, _index, exactMatch) => {
+    const normalizedName = store.name.toLowerCase();
+    const normalizedQuery = query.toLowerCase();
+ 
+    if (exactMatch) {
+        return normalizedName === normalizedQuery;
+    } else {
+        return `${normalizedName}`.indexOf(normalizedQuery) >= 0;
+    }
+};
 
   useEffect(() => {
     setLoading(true)
@@ -39,6 +54,7 @@ const Loans = () => {
   const getLoans = () => {
     axios.get(process.env.REACT_APP_API_URL + 'loans?search=' + query, authHeader).then((resp) => {
       setData(resp.data.loans);
+      setStores(resp.data.stores);
       setLoading(false);
       setDataLoading(false);
     }).catch(function (error) {
@@ -77,6 +93,37 @@ const Loans = () => {
     { id: 1, intent: 'success', label: 'Create New Loan', func: () => navigate("/loans/new")}
   ]
 
+  const renderStore = (store, { handleClick, handleFocus, modifiers, query }) => {
+    if (!modifiers.matchesPredicate) {
+        return null;
+    }
+    return (
+        <MenuItem
+            active={modifiers.active}
+            disabled={modifiers.disabled}
+            key={store.id}
+            onClick={handleClick}
+            onFocus={handleFocus}
+            text={store.name}
+        />
+    );
+  };
+
+  useEffect(() => {
+    if(selectedStore.id > 0){
+      setDataLoading(true);
+      axios.get(process.env.REACT_APP_API_URL + 'loans/filter_by_store?selected_store_id=' + selectedStore.id, authHeader).then((resp) => {
+        setData(resp.data.loans.sort((a, b) => b.id - a.id));
+        setLoading(false);
+        setDataLoading(false);
+      }).catch(function (error) {
+        if(error.code === "ERR_BAD_REQUEST"){
+          console.log("Error: ", error);
+        }
+      });
+    }
+  }, [selectedStore])
+
   return(
     <Layout 
       showBreadcrumbs={true} 
@@ -84,30 +131,49 @@ const Loans = () => {
       actions={layoutActions}>
         <Row>
           <Col lg={4}>
-            <input style={{width: '100%'}} type="search" data-testid="searchbar" className={loading ? Classes.SKELETON : ''} value={query} placeholder="Search for a Loan...." onChange={(e) => {
-              setQuery(e.target.value);
-              setDataLoading(true);
-            }} />
+            <div style={{ margin: '0px 22px' }}>
+              <input style={{width: '100%'}} type="search" data-testid="searchbar" value={query} placeholder="Search for a Loan...." onChange={(e) => {
+                setSelectedStore({});
+                setQuery(e.target.value);
+                setDataLoading(true);
+              }} />
+            </div>
           </Col>
         </Row>
 
         <Card.Body>
-          <div className={dataLoading ? Classes.SKELETON : ''}>
-            <div style={{textAlign: 'left'}}>
-              <Button intent={selectedFilter === 'all' ? 'primary' : 'default'} onClick={() => setSelectedFilter("all")} style={{marginLeft: 5}}>All</Button>
-              <Button intent={selectedFilter === 'missing' ? 'primary' : 'default'} onClick={() => setSelectedFilter("missing")} style={{marginLeft: 5}}>Missing Payments</Button>
-            </div>
-            {!loading ? (
+          <div style={{textAlign: 'left'}}>
+            <Row>
+              <Col lg={2}>
+                <Button intent={selectedFilter === 'all' ? 'primary' : 'default'} onClick={() => setSelectedFilter("all")} style={{marginLeft: 5}}>All</Button>  
+                <Button intent={selectedFilter === 'missing' ? 'primary' : 'default'} onClick={() => setSelectedFilter("missing")} style={{marginLeft: 5 }}>Missing Payments</Button>
+              </Col>
+              <Col>
+                <Select2
+                  items={stores}
+                  itemPredicate={filterStores}
+                  itemRenderer={renderStore}
+                  noResults={<MenuItem disabled={true} text="No results." />}
+                  onItemSelect={setSelectedStore}
+                  minimal={true}
+                >
+                  <Button text={selectedStore?.name || '-- select a store --'} rightIcon="double-caret-vertical" placeholder="Select a store" />
+                </Select2>
+              </Col>
+            </Row>
+          </div>
+          {!loading ? (
+            <div className={dataLoading ? Classes.SKELETON : ''} style={{marginTop: 10}}>
               <DataTable
                 columns={columns}
                 data={data}
                 selectableRows
                 pagination
               />
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
         </Card.Body>
     </Layout>
   )
